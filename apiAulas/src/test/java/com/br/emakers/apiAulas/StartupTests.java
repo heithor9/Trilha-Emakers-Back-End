6fbc8247f997a7439b82e3a03d995b3b62461f41
Heitor Ramos <heitorvieira1220@gmail.com>
2024-12-27 18:33:22 -0300

    Primeiro commit

---
+++ apiAulas/src/test/java/com/br/emakers/apiAulas/StartupTests.java
@@ -0,0 +1,13 @@
+package com.br.emakers.apiAulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
